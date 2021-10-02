9997a65df9ccdd8f654238b902e7795eb4e4930f
Vitor Pavan <vipavani@hotmail.com>
2021-10-02 11:21:12 +0200

    eslint

--- blogr-nextjs-prisma/next-env.d.ts
+++ blogr-nextjs-prisma/next-env.d.ts
@@ -4,3 +4,13 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            prisma: any
+        }
+    }
+}
+
+export {}
